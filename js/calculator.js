9a1af649785e0d9a0d8226a184254b328964508a
dipesh <dipdreaming92@gmail.com>
2017-04-22 11:15:08 -1200

    calculator added

---
+++ js/calculator.js
@@ -0,0 +1,102 @@
+window.addEventListener('load', handleLoad);
+
+var op = '+'; // The calculation operator
+
+function handleLoad() {
+    var calculateButton = document.querySelector('#calculate');
+    var clearButton = document.querySelector('#clear');
+    var minusButton = document.querySelector('#minus');
+    var plusButton = document.querySelector('#plus');
+    var multiplyButton = document.querySelector('#Multiply');
+    var divisionButton = document.querySelector('#division');
+
+
+
+    calculateButton.addEventListener('click', handleCalculateClick);
+    clearButton.addEventListener('click', handleClearClick);
+    minusButton.addEventListener('click', handleMinusClick);
+    plusButton.addEventListener('click', handlePlusClick);
+    multiplyButton.addEventListener('click', handlemultiplyClick);
+    divisionButton.addEventListener('click', handledivisionClick);
+
+}
+
+function handlePlusClick() {
+    console.log('Plus button clicked.');
+
+    var operator = document.querySelector('#operator');
+
+    op = '+';
+    operator.innerText = '+';
+}
+
+function handleMinusClick() {
+    console.log('Minus button clicked.');
+
+    var operator = document.querySelector('#operator');
+
+    op = '-';
+    operator.innerText = '-';
+}
+function handlemultiplyClick() {
+    console.log('Multiply button clicked.');
+
+    var operator = document.querySelector('#operator');
+
+    op = '*';
+    operator.innerText = '*';
+}
+function handledivisionClick() {
+    console.log('division button clicked.');
+
+    var operator = document.querySelector('#operator');
+
+    op = '/';
+    operator.innerText = '/';
+}
+function handleCalculateClick() {
+    console.log('Calculate Button Clicked.');
+
+    // Get the input & result elements
+    var input1 = document.querySelector('#input1');
+    var input2 = document.querySelector('#input2');
+    var output = document.querySelector('#output');
+
+    // Get the values.
+    var value1 = Number(input1.value);
+    var value2 = Number(input2.value);
+
+    // Just add those two values.
+    var result = calculate(value1, value2);
+
+    // Update the output element's content.
+    output.innerHTML = result;
+}
+
+function calculate(value1, value2) {
+    switch (op) {
+        case '+':
+            return value1 + value2;
+
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;        
+    }
+}
+
+function handleClearClick() {
+    console.log('Clear Button Clicked.');
+
+    // Get the input & result elements.
+    var input1 = document.querySelector('#input1');
+    var input2 = document.querySelector('#input2');
+    var output = document.querySelector('#output');
+
+    // Clear the fields and output.
+    input1.value = '';
+    input2.value = '';
+    output.innerHTML = '';
+}
